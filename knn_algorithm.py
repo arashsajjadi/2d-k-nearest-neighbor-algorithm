18749544024cde2b1e53bd981c27dba04268d4cc
Arash Sajjadi <arash.sajjadi.1@gmail.com>
2026-03-28 00:04:21 -0600

    init: Add base KNN python implementation

--- knn_algorithm.py
+++ knn_algorithm.py
@@ -3,10 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from mpl_toolkits import mplot3d
 
-def true_label(x,y):
+def true_label(x, y):
     c = 2*np.sin(x/y) + 2*np.cos(x*y) + 0.15
     return c > 0.5
-    
-# KNN Implementation and Metric evaluation
-# Full code available in this repository.
+
+# Full Implementation in the repository...
